3239168ee7d1ef72367c703329ab5b026447152f
Muni Neelam <112398733+Rohit-muni45@users.noreply.github.com>
2026-06-07 19:25:40 +0530

    Change resume link in Hero Component Changed
    Updated the resume link in the hire button to a new URL.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
         I'm a passionate Full Stack Developer with experience in building responsive and
          user-friendly web and mobile applications.
         </p>
-        <button className="hire-button" onClick={() => window.open('https://drive.google.com/file/d/1B_lvTf7tik24qOW_vgmB65IMvITKS2Rk/view?usp=sharing', '_blank')}>Resume</button>
+        <button className="hire-button" onClick={() => window.open('https://drive.google.com/file/d/1t3IlRSCO3Q3Q294SZzyy3LOlol2ZU-Mt/view?usp=sharing', '_blank')}>Resume</button>
       </div>
       <div className="hero-image">
         <div className="image-container">
